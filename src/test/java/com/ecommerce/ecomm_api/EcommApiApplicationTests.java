f515962ef9743070a5a0d282e129e1f9eaf0d652
Manojkumar <psmanojkumarv@gmail.com>
2025-09-06 21:14:12 +0530

    Initial Empty webapi

---
+++ src/test/java/com/ecommerce/ecomm_api/EcommApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
